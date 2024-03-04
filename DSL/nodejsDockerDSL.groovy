fc543ea2963181a52e5588a95069f965b9d3d7ae
xp3ctr0 <46269959+xp3ctr0@users.noreply.github.com>
2024-03-03 20:07:52 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -22,23 +22,23 @@ job('Aplicacion Node.js Docker DSL') {
             skipDecorate()
         }
     }
-    publishers {
-	slackNotifier {
-            notifyAborted(true)
-            notifyEveryFailure(true)
-            notifyNotBuilt(false)
-            notifyUnstable(false)
-            notifyBackToNormal(true)
-            notifySuccess(true)
-            notifyRepeatedFailure(false)
-            startNotification(false)
-            includeTestSummary(false)
-            includeCustomMessage(false)
-            customMessage(null)
-            sendAs(null)
-            commitInfoChoice('NONE')
-            teamDomain(null)
-            authToken(null)
-        }
-    }
+ //    publishers {
+	// slackNotifier {
+ //            notifyAborted(true)
+ //            notifyEveryFailure(true)
+ //            notifyNotBuilt(false)
+ //            notifyUnstable(false)
+ //            notifyBackToNormal(true)
+ //            notifySuccess(true)
+ //            notifyRepeatedFailure(false)
+ //            startNotification(false)
+ //            includeTestSummary(false)
+ //            includeCustomMessage(false)
+ //            customMessage(null)
+ //            sendAs(null)
+ //            commitInfoChoice('NONE')
+ //            teamDomain(null)
+ //            authToken(null)
+ //        }
+ //    }
 }
